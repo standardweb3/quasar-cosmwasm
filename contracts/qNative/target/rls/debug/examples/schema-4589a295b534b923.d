7122e793eabffb1d0dc90370086cf093dc5f75f9
hskang9 <hskang9@gmail.com>
2020-10-29 21:03:28 +0900

    update contract directory

--- contracts/qNative/target/rls/debug/examples/schema-4589a295b534b923.d
+++
@@ -1,7 +0,0 @@
-/Users/hyungsukkang/quasar-cosmwasm/contracts/qNative/target/rls/debug/examples/schema-4589a295b534b923.rmeta: examples/schema.rs
-
-/Users/hyungsukkang/quasar-cosmwasm/contracts/qNative/target/rls/debug/examples/schema-4589a295b534b923.d: examples/schema.rs
-
-examples/schema.rs:
-
-# env-dep:CARGO_PKG_NAME=q_native
